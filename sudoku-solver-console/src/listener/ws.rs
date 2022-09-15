cebaf05f803ea4bfcbcebd6ff042c031b1e5c820
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-09-15 19:52:07 +0100

    (websocket): Refactor the listener to thread the handler
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- sudoku-solver-console/src/listener/ws.rs
+++ sudoku-solver-console/src/listener/ws.rs
@@ -1,17 +1,22 @@
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+
 use super::client::Client;
 use super::Clients;
 use futures::{FutureExt, StreamExt};
 use standard_constraints::message_handler::*;
-use tokio::{runtime::Handle, sync::mpsc};
-use tokio_stream::wrappers::UnboundedReceiverStream;
+use tokio::sync::mpsc::{self, Sender};
+use tokio_stream::wrappers::ReceiverStream;
 use uuid::Uuid;
 use warp::ws::{Message, WebSocket};
 
 pub async fn client_connection(ws: WebSocket, clients: Clients) {
     let (client_ws_sender, mut client_ws_rcv) = ws.split();
-    let (client_sender, client_rcv) = mpsc::unbounded_channel();
+    let (client_sender, client_rcv) = mpsc::channel(5);
 
-    let client_rcv = UnboundedReceiverStream::new(client_rcv);
+    let client_rcv = ReceiverStream::new(client_rcv);
 
     tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
         if let Err(e) = result {
@@ -23,12 +28,15 @@ pub async fn client_connection(ws: WebSocket, clients: Clients) {
 
     let new_client = Client {
         client_id: uuid.clone(),
-        sender: Some(client_sender),
+        sender: None,
     };
 
     clients.lock().await.insert(uuid.clone(), new_client);
 
     println!("Client {} connected", uuid);
+
+    let mut handler = ThreadedHandler::new(client_sender.clone()).await;
+
     while let Some(result) = client_ws_rcv.next().await {
         let msg = match result {
             Ok(msg) => msg,
@@ -37,54 +45,109 @@ pub async fn client_connection(ws: WebSocket, clients: Clients) {
                 break;
             }
         };
-        client_msg(&uuid, msg, &clients).await;
+
+        if !handler.is_ready() {
+            handler.cancel().await;
+            handler = ThreadedHandler::new(client_sender.clone()).await;
+        }
+
+        if handler.send(msg).await.is_err() {
+            break;
+        }
     }
+
+    handler.cancel().await;
 
     clients.lock().await.remove(&uuid);
     println!("Client {} Disconnected", uuid);
 }
 
-struct SendResultWS {
-    clients: Clients,
-    client_id: String,
+struct SendResultForWS {
+    sender: Sender<Result<Message, warp::Error>>,
 }
 
-impl SendResultWS {
-    fn new(clients: Clients, client_id: String) -> Self {
-        Self { clients, client_id }
+impl SendResultForWS {
+    fn new(sender: Sender<Result<Message, warp::Error>>) -> Self {
+        Self { sender }
     }
 }
 
-impl SendResult for SendResultWS {
+impl SendResult for SendResultForWS {
+    /// Panics if we cannot send, which will terminate the thread
     fn send_result(&mut self, result: &str) {
-        let handle = Handle::current();
-        let _ = handle.enter();
-        futures::executor::block_on(async {
-            let locked = self.clients.lock().await;
-            match locked.get(&self.client_id) {
-                Some(v) => {
-                    if let Some(sender) = &v.sender {
-                        let _ = sender.send(Ok(Message::text(result)));
-                    }
+        self.sender
+            .blocking_send(Ok(Message::text(result)))
+            .unwrap();
+    }
+}
+
+struct ThreadedHandler {
+    join_handle: std::thread::JoinHandle<()>,
+    sender: Sender<Message>,
+    cancel_token: Arc<AtomicBool>,
+    ready_token: Arc<AtomicBool>,
+}
+
+impl ThreadedHandler {
+    async fn new(client_sender: Sender<Result<Message, warp::Error>>) -> Self {
+        let (handler_sender, mut handler_recv) = mpsc::channel::<Message>(5);
+        let cancel_token = Arc::new(AtomicBool::from(false));
+        let ready_token = Arc::new(AtomicBool::new(true));
+
+        let joiner = std::thread::spawn({
+            let cancel_token = Arc::clone(&cancel_token);
+            let ready_token = Arc::clone(&ready_token);
+            move || {
+                // This is the thread for handling messages from the client.
+                // We handle multiple messages before we give up
+                let mut message_handler =
+                    MessageHandler::new(Box::new(SendResultForWS::new(client_sender)));
+                while let Some(message) = handler_recv.blocking_recv() {
+                    let message = match message.to_str() {
+                        Ok(v) => v.to_string(),
+                        Err(_) => break,
+                    };
+                    ready_token.store(false, Ordering::SeqCst);
+                    message_handler.handle_message(&message);
+                    ready_token.store(true, Ordering::SeqCst);
                 }
-                None => return,
             }
         });
+
+        ThreadedHandler {
+            join_handle: joiner,
+            sender: handler_sender,
+            cancel_token,
+            ready_token,
+        }
     }
-}
 
-async fn client_msg(client_id: &str, msg: Message, clients: &Clients) {
-    let message = match msg.to_str() {
-        Ok(v) => v.to_string(),
-        Err(_) => return,
-    };
+    async fn send(&self, message: Message) -> Result<(), mpsc::error::SendError<Message>> {
+        self.sender.send(message).await
+    }
 
-    let client_id = client_id.to_owned();
-    let clients = clients.clone();
-    let _ = tokio::task::spawn_blocking(move || {
-        let send_result = Box::new(SendResultWS::new(clients, client_id));
-        let mut message_handler = MessageHandler::new(send_result);
-        message_handler.handle_message(&message);
-    })
-    .await;
+    async fn cancel(self) {
+        let Self {
+            join_handle,
+            sender,
+            cancel_token,
+            ready_token,
+        } = self;
+        cancel_token.store(true, Ordering::SeqCst);
+        drop(sender);
+        drop(ready_token);
+        let mut counter = 0;
+        const MAX_COUNT: usize = 100;
+        while counter < MAX_COUNT && !join_handle.is_finished() {
+            tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+            counter += 1;
+        }
+        if counter == MAX_COUNT {
+            println!("Handler thread didn't stop in time, CPU may be wasted for a while");
+        }
+    }
+
+    fn is_ready(&self) -> bool {
+        self.ready_token.load(Ordering::SeqCst)
+    }
 }
