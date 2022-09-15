3991012603f7727ed7815cb343e671dcc8d9b73a
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-09-15 21:05:12 +0100

    (cancellation): Add rudimentary cancellation into the solver
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- standard-constraints/src/chess_constraint.rs
+++ standard-constraints/src/chess_constraint.rs
@@ -112,7 +112,7 @@ mod test {
             .build()
             .unwrap();
 
-        let solution_count = solver.find_solution_count(10000, None);
+        let solution_count = solver.find_solution_count(10000, None, None);
         assert!(solution_count.is_exact_count());
         assert_eq!(solution_count.count().unwrap(), 4);
     }
@@ -132,7 +132,7 @@ mod test {
                 .build()
                 .unwrap();
 
-            let solution_count = solver.find_solution_count(10000, None);
+            let solution_count = solver.find_solution_count(10000, None, None);
             assert!(solution_count.is_exact_count());
             assert_eq!(solution_count.count().unwrap(), 1);
         }
