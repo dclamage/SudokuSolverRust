6c38b6613a45fe9865d4bb6ba1930a6b1a0cf23a
David Clamage <dclamage@users.noreply.github.com>
2022-09-17 23:20:26 -0700

    Fix for wasm crash due to using Instant::now
    Added way to host the wasm via node for local testing.
     - This includes changing the js and wasm files to point to localhost.
      - This will need to be changed so that the build system uses the appropriate URL based on the desired context.

--- user-scripts/fpuzzles-sudokusolver-wasm-worker.js
+++ user-scripts/fpuzzles-sudokusolver-wasm-worker.js
@@ -1,9 +1,9 @@
-importScripts('https://sudokusolverwasm.s3.us-west-2.amazonaws.com/sudoku_solver_wasm.js');
+importScripts('http://localhost:3000/sudoku_solver_wasm.js');
 
 const { solve } = wasm_bindgen;
 
 async function init_wasm_worker() {
-    await wasm_bindgen("https://sudokusolverwasm.s3.us-west-2.amazonaws.com/sudoku_solver_wasm_bg.wasm");
+    await wasm_bindgen("http://localhost:3000/sudoku_solver_wasm_bg.wasm");
     self.onmessage = async event => {
         solve(event.data, response => self.postMessage(response));
     }
